60bb6d998e752f6cfc130dad073dfa38731b139e
pedropmoraesf <pedro.pmoraesf@gmail.com>
2026-09-16 21:27:24 -0300

    Adiciona tipos de meteoros e bônus

---
+++ src/br/grassinimoraes/divasteroides/MeteorType.java
@@ -0,0 +1,14 @@
+package br.grassinimoraes.divasteroides;
+
+enum MeteorType {
+    NORMAL,
+    ADDITION,
+    MULTIPLICATION,
+    BONUS_AMMO,
+    BONUS_SUBTRACTOR,
+    BONUS_MONEY,
+    BONUS_HEALTH,
+    BONUS_SHIELD,
+    BONUS_ZERO,
+    BONUS_NUKE
+}
